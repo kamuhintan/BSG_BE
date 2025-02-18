8a34b07b3fbd34f8f2cde1abb41311d02a014cfc
kamuhintan <kamuhintan@gmail.com>
2025-02-18 19:46:59 +0800

    first commit

---
+++ src/main/java/com/bsg/app/BsgApplication.java
@@ -0,0 +1,13 @@
+package com.bsg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsg/app/BsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsg.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
