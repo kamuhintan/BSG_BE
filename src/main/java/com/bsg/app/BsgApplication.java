d911d05bcc5727d07eb8c81af6bd1e078fbe935d
kamuhintan <kamuhintan@gmail.com>
2025-02-19 20:58:56 +0800

    setup security

--- src/main/java/com/bsg/app/BsgApplication.java
+++ src/main/java/com/bsg/app/BsgApplication.java
@@ -2,6 +2,8 @@ package com.bsg.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BsgApplication {
@@ -9,5 +11,8 @@ public class BsgApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BsgApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
